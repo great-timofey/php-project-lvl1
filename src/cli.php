569d09aa7c08013e03412043cd1bd238c1e708db
Timofei Tverdokhlebov <great.timofey@icloud.com>
2019-09-13 01:13:53 +0700

    add php-cli-tools, add autoload

---
+++ src/cli.php
@@ -0,0 +1,15 @@
+<?php
+namespace Braingames\Cli;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run()
+{
+  line("Welcome to the Brain Games!\n");
+  $name = prompt("May I have your name?");
+  line("Hello, {$name}!");
+}
+
